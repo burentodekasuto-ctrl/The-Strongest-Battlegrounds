7be6bb3f94bb2146c5adc2d0be076becec1cb07e
burentodekasuto-ctrl <burentodekasuto@gmail.com>
2026-05-24 14:26:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CHARACTERS = {
+    saitama: { name: "SAITAMA", suit: 0xffcc00, arms: 0xff3333, fx: 0xffaa00, nameTag: "Saitama" },
+    garou: { name: "GAROU", suit: 0x222226, arms: 0xeeeeee, fx: 0x9900ff, nameTag: "Garou" },
+    genos: { name: "GENOS", suit: 0x3a3d40, arms: 0xffdd44, fx: 0x00ffff, nameTag: "Genos" }
+};
